cc2f82d6589e734f9a7c23d660cc702e2ae94be9
Katherine Lennox <klennox24@biomedscienceacademy.org>
2020-11-09 23:58:48 +0000

    'adjective_one' Variable Input

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ Period/Core: 6
 """
 
 #(name_one) is going to (place_one) with (name_two).
-#"(place_one) is almost like a (adjetive_one) version of
+#"(place_one) is almost like a (adjective_one) version of
 #(place_two)" mentions (name_one).
 
-#(name_one) (verb_ending_in_ed) up to (place_one) to take a (adjetive_two)
+#(name_one) (verb_ending_in_ed) up to (place_one) to take a (adjective_two)
 #picture. Suddenly, (name_two) spotted a (animal).
 #(name_two) said, "(name_one) lets go pet that (animal)!"
 
 #after (name_one) and (name_two) were done petting the animal,
-#(name_one) noticed a (adjetive_three) cafe. (name_one) suggested,
-#"Lets go over there!" (name_one) and (name_two) (verb_ ending_ed) to the (adjetive_three) cafe.
+#(name_one) noticed a (adjective_three) cafe. (name_one) suggested,
+#"Lets go over there!" (name_one) and (name_two) (verb_ ending_ed) to the (adjective_four) cafe.
 
 #(name_one) and (name_two) reached the cafe, and ate some
-#(adjetive_four) (food). (name_two) exclaimed, "This was so
-#(adjetive_five) !"
+#(adjective_five) (food). (name_two) exclaimed, "This was so
+#(adjective_six) !"
 
 print("Let's play Silly Sentences!")
 
@@ -29,3 +29,5 @@ name_one = input("Enter a name: ")
 name_two = input("Enter a second name: ")
 
 place_one = input("Enter a place: ")
+
+adjective_one = input("Enter an adjective: ")
