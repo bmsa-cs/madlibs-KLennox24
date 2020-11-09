52e70d4f4dca452b2b234b68c684d0176447b6a4
Katherine Lennox <klennox24@biomedscienceacademy.org>
2020-11-09 23:40:18 +0000

    Test Push

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Katie Lennox
+Period/Core: 6
 
 
 """
+
+#Template: (name) is going to (place) with (name 2).
+#(place) is almost like a (adjetive) version of
+#(place 2)
+
+#(name 1) (action ending in ed) up to (place) to take a (adjetive)
+#picture. Suddenly, (name 2) spotted a (animal).
+#(name 2) said, "(name 1) lets go pet that (animal)!"
+
+#after (name 1) and (name 2) were done petting the animal,
+#(name 1) noticed a (adjetive) cafe. (name 1) suggested,
+#"Lets go over there!" (name 1) and (name 2) (action ending in ed)
+
+#(name 1) and (name 2) reached the cafe, and ate some
+#(adjetive) (food). (name 2) exclaimed, "This was so
+#(adjetive) !"
+
+print("something")
