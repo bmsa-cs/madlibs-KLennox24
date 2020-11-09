03ebd66845ce8e684461a3d10403413b06d1208f
Katherine Lennox <klennox24@biomedscienceacademy.org>
2020-11-09 23:52:57 +0000

    Final Template & Beginning Print Command

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ Period/Core: 6
 
 """
 
-#Template: (name_one) is going to (place_one) with (name_two)
-#(place_one) is almost like a (adjetive_one) version of
-#(place_two)
+#(name_one) is going to (place_one) with (name_two).
+#"(place_one) is almost like a (adjetive_one) version of
+#(place_two)" mentions (name_one).
 
-#(name_one) (action_ending_in_ed) up to (place_one) to take a (adjetive_two)
+#(name_one) (verb_ending_in_ed) up to (place_one) to take a (adjetive_two)
 #picture. Suddenly, (name_two) spotted a (animal).
 #(name_two) said, "(name_one) lets go pet that (animal)!"
 
 #after (name_one) and (name_two) were done petting the animal,
 #(name_one) noticed a (adjetive_three) cafe. (name_one) suggested,
-#"Lets go over there!" (name_one) and (name_two) (action_ ending_ed)
+#"Lets go over there!" (name_one) and (name_two) (verb_ ending_ed) to the (adjetive_three) cafe.
 
 #(name_one) and (name_two) reached the cafe, and ate some
 #(adjetive_four) (food). (name_two) exclaimed, "This was so
 #(adjetive_five) !"
 
+print("Let's play Silly Sentences!")
